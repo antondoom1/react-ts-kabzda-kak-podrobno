d247c7fa960ff954957a24c45f30c89d532f8d9a
AntonYakavenka <doomanton@gmail.com>
2022-05-06 13:55:13 +0300

    uncontrolled input, controlled input, checkbox, select

--- src/components/UncontrolledOnOff/UncontrolledOnOff.stories.tsx
+++ src/components/UncontrolledOnOff/UncontrolledOnOff.stories.tsx
@@ -14,4 +14,3 @@ export const OnMode = () => <UncontrolledOnOff defaultOn={true} onChange={callba
 export const OffMode = () => <UncontrolledOnOff defaultOn={false} onChange={callback}/>
 export const BugMode = () => <div>Unsync when change defaultValue when already rendered</div>
 
-export const DefaultInputValue = () => <input defaultValue={'yo'} />
