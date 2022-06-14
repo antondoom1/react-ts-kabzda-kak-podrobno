818e3c9291cd719e6fd705aa8457a35548a869e5
AntonYakavenka <doomanton@gmail.com>
2022-06-14 17:50:22 +0300

    useEffect, cleanup

--- src/stories/UseEffect.stories.tsx
+++ src/stories/UseEffect.stories.tsx
@@ -37,3 +37,67 @@ export const SimpleExample = () => {
   </>
 }
 
+export const ResetEffectExample = () => {
+  const [counter, setCounter] = useState(1)
+
+  console.log('Component rendered with ' + counter)
+
+  useEffect(() => {
+    console.log('Effect occurred' + counter)
+
+    return () => {
+      console.log('Reset Effect' + counter)
+    }
+  }, [])
+
+  const increase = () => setCounter(counter + 1)
+
+  return <>
+    Hello, counter: {counter}
+    <button onClick={increase}>+</button>
+  </>
+}
+
+export const KeysTrackerExample = () => {
+  const [text, setText] = useState('')
+
+  console.log('Component rendered with ' + text)
+
+  useEffect(() => {
+    const handler = (e: KeyboardEvent) => {
+      console.log(e.key)
+      setText(text + e.key)
+    }
+
+    window.addEventListener('keypress', handler)
+
+    return () => {
+      window.removeEventListener('keypress', handler)
+    }
+  }, [text])
+
+  return <>
+    Typed text: {text}
+  </>
+}
+
+export const SetTimeoutExample = () => {
+  const [text, setText] = useState('')
+
+  console.log('Component rendered with ' + text)
+
+  useEffect(() => {
+
+    const timeoutId = setTimeout(() => {
+      setText('3 seconds passed')
+    }, 3000)
+
+    return () => {
+      clearTimeout(timeoutId)
+    }
+  }, [text])
+
+  return <>
+    Typed text: {text}
+  </>
+}
